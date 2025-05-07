f23daa082921a98284d67027b97381c4a57552be
KaldX <184607251+KaldX@users.noreply.github.com>
2025-05-07 23:22:43 +0800

    Revert "登录注册 主界面开发"
    This reverts commit 6c49f2ceef1ca8c8d4ee42bc2858ebd2f4f5ab66.

--- frontend/src/layouts/AuthMasterLayout.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div class="min-h-screen bg-gradient-to-br from-slate-100 via-gray-50 to-sky-100 flex items-center justify-center">
-    <!-- 添加 flex items-center justify-center 使 AuthLayout.vue 中的内容居中 -->
-    <router-view />
-  </div>
-</template>
-
-<script setup>
-</script>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,40 +1,4 @@
 import { createApp } from 'vue'
-import './styles/index.css'
 import App from './App.vue'
-import router from './router'
-import pinia from './store'
-import { MotionPlugin } from '@vueuse/motion'
 
-import PrimeVue from 'primevue/config';
-import Button from 'primevue/button';
-import InputText from 'primevue/inputtext';
-import Password from 'primevue/password';
-import FloatLabel from 'primevue/floatlabel';
-import Checkbox from 'primevue/checkbox';
-
-const app = createApp(App)
-
-app.use(router)
-app.use(pinia)
-app.use(MotionPlugin)
-
-// 配置 PrimeVue，并设置 theme 为 'none'； 目前显示有点问题，之后会调整
-app.use(PrimeVue, {
-  theme: 'none'
-  // ripple: true, // 涟漪效果
-  // inputStyle: 'filled' // 设置全局输入框样式，如 'filled' 或 'outlined'
-});
-
-// 全局注册的组件
-app.component('Button', Button);
-app.component('InputText', InputText);
-app.component('Password', Password);
-app.component('FloatLabel', FloatLabel);
-app.component('Checkbox', Checkbox);
-// app.directive('ripple', Ripple);
-// app.directive('tooltip', Tooltip);
-
-app.mount('#app')
-
-
-
+createApp(App).mount('#app')
